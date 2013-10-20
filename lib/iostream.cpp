b407e6db870d35039eeef03f9268eed445e60ca4
Sergei Lodyagin <lodyagin@gmail.com>
2013-10-20 22:34:55 +0000

    _terminal_out_streambuf is added

--- lib/iostream.cpp
+++ lib/iostream.cpp
@@ -16,6 +16,28 @@
 
 namespace std {
 
+
+/**
+  * Output to a terminal.
+  * @author Sergei Lodyagin
+  */
+template <
+  class CharT,
+  class Traits = std::char_traits<CharT>
+>
+class _terminal_out_streambuf 
+  : public basic_streambuf<CharT, Traits>
+{
+protected:
+  streamsize xsputn
+    (const char_type* s, std::streamsize count) override
+  {
+    b_output_chars(s, count);
+    return count;
+  }
+};
+
+
 // FIXME std::atomic, see http://stackoverflow.com/questions/10521263/standard-way-to-implement-initializer-like-ios-baseinit
 static unsigned int nifty_counter;
 
@@ -23,7 +45,7 @@ ios_base::Init::Init()
 {
   if (nifty_counter++ == 0)
   {
-    new(&cout) ostream(new streambuf());
+    new(&cout) ostream(new _terminal_out_streambuf());
   }
 }
 
